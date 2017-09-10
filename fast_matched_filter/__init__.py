824bb56d83eeda85dfae1926ef2188e246f22cdd
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2017-09-10 21:37:35 +0000

    Remove submodule from namespace

--- fast_matched_filter/__init__.py
+++ fast_matched_filter/__init__.py
@@ -1,7 +1,15 @@
 """
 init file for fast_matched_filter library
+
+:copyright:
+    William B. Frank and Eric Beauce
+:license:
+    GNU General Public License, Version 3
+    (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 
 from .fast_matched_filter import matched_filter, test_matched_filter
+
+del fast_matched_filter
 
 __all__ = [matched_filter, test_matched_filter]
