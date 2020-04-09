8785d9b41fa6b490d395eaf96482259eda6e2255
beridel <9719801+beridel@users.noreply.github.com>
2020-04-09 09:57:41 -0700

    initial implementation of precise CPU matched-filter

--- fast_matched_filter/src/matched_filter_CPU.h
+++ fast_matched_filter/src/matched_filter_CPU.h
@@ -3,3 +3,6 @@ float network_corr(float*, int*, float*, double*, float*, int, int, int, int);
 float corrc(float*, float*, double*, int);
 void cumsum_square_data(float*, int, float*, int, int, double*);
 void neumaier_cumsum_squared(float*, int, double*);
+void matched_filter_precise(float*, int*, float*, float*, int, int, int, int, int, int, int, float*);
+float network_corr_precise(float*, float*, int*, float*, float*,int, int, int, int);
+float corrc_precise(float*, float, float*, int);
