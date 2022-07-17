b0b0fb95fc94ecab70ffce8d429b43c58b8d71b3
Eric Beauce <ebeauce@hypo-8.ldeo.columbia.edu>
2022-07-17 15:16:44 -0400

    CPU implementation of no-sum CCs

--- fast_matched_filter/src/matched_filter_CPU.h
+++ fast_matched_filter/src/matched_filter_CPU.h
@@ -1,13 +1,61 @@
-void matched_filter(float*, float*, int*, float*, float*,
-                    size_t, size_t, size_t, size_t, size_t, size_t, size_t, float*);
-float network_corr(float*, float*, int*, float*, double*,
-                   float*, size_t, size_t, size_t, size_t);
-float corrc(float*, float, float*, double*, size_t);
-void cumsum_square_data(float*, size_t, float*, size_t, size_t, double*);
-void neumaier_cumsum_squared(float*, size_t, double*);
-void matched_filter_precise(float*, float*, int*, float*, float*,
-                            size_t, size_t, size_t, size_t, size_t,
-                            size_t, size_t, float*, int);
-float network_corr_precise(float*, float*, int*, float*, float*,
-                           size_t, size_t, size_t, size_t, int);
-float corrc_precise(float*, float, float*, size_t, int);
+void matched_filter(
+        float*, float*, int*, float*, float*,
+        size_t, size_t, size_t, size_t, size_t,
+        size_t, size_t, float*
+        );
+float network_corr(
+        float*, float*, int*, float*, double*,
+        float*, size_t, size_t, size_t, size_t
+        );
+float corrc(
+        float*, float, float*, double*, size_t
+        );
+
+void matched_filter_precise(
+        float*, float*, int*, float*, float*,
+        size_t, size_t, size_t, size_t, size_t,
+        size_t, size_t, float*, int
+        );
+float network_corr_precise(
+        float*, float*, int*, float*, float*,
+        size_t, size_t, size_t, size_t, int
+        );
+float corrc_precise(
+        float*, float, float*, size_t, int
+        );
+
+void matched_filter_no_sum(
+        float*, float*, int*, float*, float*,
+        size_t, size_t, size_t, size_t, size_t,
+        size_t, size_t, float*
+        );
+void network_corr_no_sum(
+        float*, float*, int*, float*, double*,
+        float*, size_t, size_t, size_t, size_t,
+        float*
+        );
+float corrc_no_sum(
+        float*, float, float*, double*, size_t
+        );
+
+void matched_filter_precise_no_sum(
+        float*, float*, int*, float*, float*,
+        size_t, size_t, size_t, size_t, size_t,
+        size_t, size_t, float*, int
+        );
+void network_corr_precise_no_sum(
+        float*, float*, int*, float*, float*,
+        size_t, size_t, size_t, size_t, int,
+        float*
+        );
+float corrc_precise_no_sum(
+        float*, float, float*, size_t, int
+        );
+
+void cumsum_square_data(
+        float*, size_t, float*, size_t, size_t, double*
+        );
+void neumaier_cumsum_squared(
+        float*, size_t, double*
+        );
+
