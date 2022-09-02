37a11395654496e828d624d1e90542dac317957e
Eric Beauce <ebeauce@mit.edu>
2022-09-02 13:10:43 -0400

    Implement non summed cc on GPU (1)

--- fast_matched_filter/src/matched_filter_GPU.h
+++ fast_matched_filter/src/matched_filter_GPU.h
@@ -1,4 +1,4 @@
 void matched_filter(
         float*, float*, int*, float*, float*,
         size_t, size_t, size_t, size_t, size_t,
-        size_t, size_t, float*, int);
+        size_t, size_t, float*, int, int);
